8e603dd878481cb2b034c2e608393fa393d75e17
jpprogramerdev <jpgertto.1204fernadesz@gmail.com>
2025-10-04 14:50:15 -0300

    feat: endpoint todos categoria prato

---
+++ APIGastroLink/Controllers/CategoriaPratoController.cs
@@ -0,0 +1,24 @@
+﻿using APIGastroLink.DAO.Interface;
+using APIGastroLink.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIGastroLink.Controllers {
+    [ApiController]
+    [Route("api-gastrolink/categoria-prato")]
+    public class CategoriaPratoController : ControllerBase {
+        private IDAOCategoriaPrato _daoCategoriaPrato;
+
+        public CategoriaPratoController(IDAOCategoriaPrato daoCategoriraPrato) {
+            _daoCategoriaPrato = daoCategoriraPrato;
+        }
+
+        //GET api-gastrolink/categoria-prato
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoriaPrato>>> GetCategoriasPrato() {
+            var categoriasPrato = (await _daoCategoriaPrato.SelectAll()).Cast<CategoriaPrato>().ToList();
+
+            return Ok(categoriasPrato);
+        }
+    }
+}
---
+++ APIGastroLink/DAO/DAOCategoriaPrato.cs
@@ -0,0 +1,32 @@
+﻿using APIGastroLink.Context;
+using APIGastroLink.DAO.Interface;
+using APIGastroLink.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIGastroLink.DAO {
+    public class DAOCategoriaPrato : IDAOCategoriaPrato {
+        private readonly AppDbContext _context;
+
+        public DAOCategoriaPrato(AppDbContext context) {
+            _context = context;
+        }
+
+        public Task Delete(EntidadeDominio entidadeDominio) {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(EntidadeDominio entidadeDominio) {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<EntidadeDominio>> SelectAll() => await _context.CategoriasPratos.ToListAsync();
+
+        public Task<EntidadeDominio> SelectById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(EntidadeDominio entidadeDominio) {
+            throw new NotImplementedException();
+        }
+    }
+}
