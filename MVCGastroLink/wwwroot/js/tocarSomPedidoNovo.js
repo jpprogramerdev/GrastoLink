117d2047e3d56ad510a4f36759cf489089883bbd
jpgerotto <jpgertto.1204fernadesz@gmail.com>
2025-12-25 20:53:35 -0300

    feat: modulo de notificação quando chega um pedido novo

---
+++ MVCGastroLink/wwwroot/js/tocarSomPedidoNovo.js
@@ -0,0 +1,8 @@
+﻿function tocarSomNovoPedido() {
+    const audio = document.getElementById('pedidoNovoSound');
+
+    if (audioLiberado) {
+        audio.currentTime = 0;
+        audio.play().catch(err => console.warn("Falha ao tocar som:", err));
+    }
+}
